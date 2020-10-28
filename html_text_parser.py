518344caab071cf8303909b496791b01dda6196b
Blessonpx <36164387+Blessonpx@users.noreply.github.com>
2020-10-28 23:07:05 +0530

    Update html_text_parser.py

--- html_text_parser.py
+++ html_text_parser.py
@@ -11,3 +11,11 @@ path = Path(str_path)
 h=open(path)
 strText = h.read()
 soup=BeautifulSoup(strText,'html-parser')
+
+
+## Alternate 
+import re
+
+s = 'asdf=5;iwantthis123jasd'
+result = re.search('asdf=5;(.*)123jasd', s)
+print(result.group(1))
