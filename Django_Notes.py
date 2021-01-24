bc8d1881870936e4b68bc900d76ddb78b06a5fd5
Blessonpx <36164387+Blessonpx@users.noreply.github.com>
2021-01-24 21:54:18 +0530

    Create Django_Notes.py

---
+++ Django_Notes.py
@@ -0,0 +1,22 @@
+### Setup New Environment 
+python -m venv myenv
+### Install Version
+pip install django==2.0.7
+### List of Packeages initally installed
+appdirs==1.4.4
+asgiref==3.2.10
+distlib==0.3.1
+Django==3.1
+filelock==3.0.12
+mysqlclient==1.4.6
+psycopg2==2.8.5
+pytz==2020.1
+six==1.15.0
+sqlparse==0.3.1
+virtualenv==20.0.30
+### Find WHere Python Installed 
+which python3
+### Create Project Django
+django-admin startproject <Name> .
+### Run Server to Access http:/ ip
+python manage.py runserver
